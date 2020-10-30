3d97aa01bebe3e7a96004ab40b0f5912b9186ea7
Michael <mlangley@trhc.com>
2020-10-29 21:23:48 -0400

    init

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const tailwindcss = require(`tailwindcss`)
+
+module.exports = {
+  plugins: [
+    tailwindcss(`./tailwind.config.js`),
+    // If you want to use autoprefixer, use a pre v8 version or wait for the gatsby plugin to be updated for v8 plugins
+    //require(`autoprefixer`),
+    require(`cssnano`)({
+      preset: `default`,
+    }),
+  ],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    content: ["./src/**/*.html", "./src/**/*.tsx"],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
